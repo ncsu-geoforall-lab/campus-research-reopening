7a0f011f11048a473743730a1784ee1e353ea6fe
Vaclav Petras <wenzeslaus@gmail.com>
2020-05-28 15:17:20 -0400

    Clip data for easy/no zoom

--- grass_process.sh
+++ grass_process.sh
@@ -7,24 +7,29 @@ set -x
 
 SPATIAL_INPUT="$1"
 TABULAR_INPUT="$2"
-OUTPUT="$3"
+CLIP_REGION="$3"
+OUTPUT="$4"
 
 # Import data
-v.import "$SPATIAL_INPUT" output=raw_buildings 
+v.import "$SPATIAL_INPUT" output=raw_buildings
 db.in.ogr "$TABULAR_INPUT" output=building_counts_table
+v.import "$CLIP_REGION" output=region
+
+# Clip to the area of interest
+v.clip input=raw_buildings clip=region output=buildings
 
 # Join the tables
-v.db.join raw_buildings column=BLDGNUM other_table=building_counts_table other_column=Number
+v.db.join buildings column=BLDGNUM other_table=building_counts_table other_column=Number
 
 # Make Count an integer
-v.db.renamecolumn raw_buildings column=Count,count_text
-v.db.addcolumn raw_buildings columns="Count integer"
-v.db.update raw_buildings col=Count qcol="CAST(count_text AS integer)"
+v.db.renamecolumn buildings column=Count,count_text
+v.db.addcolumn buildings columns="Count integer"
+v.db.update buildings col=Count qcol="CAST(count_text AS integer)"
 
 # Replace zeros with NULLs
-v.db.update raw_buildings column=Count query_column="NULL" where="Count = 0"
+v.db.update buildings column=Count query_column="NULL" where="Count = 0"
 
 # Export data
 # Using overwrite to behave like other tools in case of re-running this.
 # (Consistent behavior if we are in a temporary location.)
-v.out.ogr input=raw_buildings output="$OUTPUT" format=GeoJSON -s --overwrite
+v.out.ogr input=buildings output="$OUTPUT" format=GeoJSON -s --overwrite
--- process.sh
+++ process.sh
@@ -7,6 +7,8 @@ set -x
 SPATIAL_INPUT="/vsizip/./data/BLDGSCAMPUS.zip/BLDGSCAMPUS.shp"
 TABULAR_INPUT="data/CGA_052620.xlsx"
 
+CLIP_REGION="data/main_region.gpkg"
+
 GRASS_OUTPUT="buildings_grass.geojson"
 OUTPUT="buildings.geojson"
 
@@ -14,6 +16,8 @@ xlsx2csv "$TABULAR_INPUT" > "raw_building_counts.csv"
 ./fix_building_number.py "raw_building_counts.csv" "building_counts_corrected.csv"
 
 grass --tmp-location "EPSG:3358" --exec \
-    ./grass_process.sh $SPATIAL_INPUT "building_counts_corrected.csv" $GRASS_OUTPUT
+    ./grass_process.sh
+        $SPATIAL_INPUT "building_counts_corrected.csv" \
+        $CLIP_REGION $GRASS_OUTPUT
 
 ogr2ogr -wrapdateline -lco "COORDINATE_PRECISION=6" -f GeoJSON -t_srs "EPSG:4326" $OUTPUT $GRASS_OUTPUT
