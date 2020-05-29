fc96e50f065996bcdab6e0b15511b4de0c2f407e
Vaclav Petras <wenzeslaus@gmail.com>
2020-05-29 13:03:31 -0400

    Fix Python formatting

--- create_keplergl.py
+++ create_keplergl.py
@@ -5,12 +5,14 @@ from keplergl import KeplerGl
 from in_place import InPlace
 from jinja2 import Template
 
+
 def read_configuration(file_name, **kwargs):
     with open(file_name) as file:
         text = file.read()
     template = Template(text)
     text = template.render(**kwargs)
     return json.loads(text)
+
 
 def main():
     config_file = "keplergl_config.json"
@@ -24,7 +26,10 @@ def main():
     # Add map creator
     with InPlace(output) as file:
         for line in file:
-            line = line.replace("<title>Kepler.gl</title>", "<title>Campus Research Reopening Map by NCSU CGA</title>")
+            line = line.replace(
+                "<title>Kepler.gl</title>",
+                "<title>Campus Research Reopening Map by NCSU CGA</title>",
+            )
             line = line.replace("Kepler.gl Jupyter", "Kepler.gl Map by NCSU CGA")
             file.write(line)
 
