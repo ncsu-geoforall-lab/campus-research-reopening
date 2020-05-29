da7d1c3289f7358e907c8f5c4a9e7600a1f99ba5
Vaclav Petras <wenzeslaus@gmail.com>
2020-05-29 12:56:27 -0400

    Customize the JSON and output HTML
    Use Jinja templates to put the right names into the map and in_place to add title and ack into the HTML.

--- create_keplergl.py
+++ create_keplergl.py
@@ -2,14 +2,31 @@
 
 import json
 from keplergl import KeplerGl
+from in_place import InPlace
+from jinja2 import Template
 
+def read_configuration(file_name, **kwargs):
+    with open(file_name) as file:
+        text = file.read()
+    template = Template(text)
+    text = template.render(**kwargs)
+    return json.loads(text)
 
 def main():
-    kepler = KeplerGl()
-    kepler.add_data(data=open("buildings.geojson").read(), name="data1")
     config_file = "keplergl_config.json"
-    kepler.config = json.loads(open(config_file).read())
-    kepler.save_to_html(file_name="keplergl.html")
+    data_id = "buildings"
+    config = read_configuration(config_file, label="Buildings", data_id=data_id)
+    kepler = KeplerGl(config=config)
+    kepler.add_data(data=open("buildings.geojson").read(), name=data_id)
+    output = "keplergl.html"
+    kepler.save_to_html(file_name=output)
+
+    # Add map creator
+    with InPlace(output) as file:
+        for line in file:
+            line = line.replace("<title>Kepler.gl</title>", "<title>Campus Research Reopening Map by NCSU CGA</title>")
+            line = line.replace("Kepler.gl Jupyter", "Kepler.gl Map by NCSU CGA")
+            file.write(line)
 
 
 if __name__ == "__main__":
